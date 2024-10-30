a48f111a03c7129692d9ec569ea9f55624c8a10b
Lors Kushtov <lors.kushtov@gmail.com>
2024-10-30 16:03:07 -0400

    Updated metadata and tag information

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'lorsk', // Site author
+	title: 'The Ingush Connection', // Site title.
+	description: 'A blog about Ingush and its culture, past and present', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
